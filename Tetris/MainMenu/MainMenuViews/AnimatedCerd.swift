fab6ee8c6358add4d5c2f8432f5605e240f5f8d9
Pavel Maslov <pavel_maslov_05@mail.ru>
2023-11-10 13:28:05 +0300

    Added gradient animation to game logo in main menu

---
+++ Tetris/MainMenu/MainMenuViews/AnimatedCerd.swift
@@ -0,0 +1,18 @@
+//
+//  AnimatedCerd.swift
+//  Tetris
+//
+//  Created by Out East on 10.11.2023.
+//
+
+import SwiftUI
+
+struct AnimatedCerd: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimatedCerd()
+}
